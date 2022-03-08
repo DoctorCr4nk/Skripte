0e24bdcabf3cc93cf9b1ed9b039debf5a40c2300
Alexander Mueller <amueller@doctorcrank.de>
2022-03-08 20:37:19 +0100

    Create template for scripts

---
+++ template.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Author:       Alexander Mueller
+# Email:        amueller@doctorcrank.de
+# Version:      0.1
+# Date:         2022-03-08
+# Comment:      Please modify this accordingly
+# Description:  This is just a template for scripts
+
+echo "Check out https://github.com/DoctorCr4nk/Skripte!"
