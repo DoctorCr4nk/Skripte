ac10c6dd724139c0207b26d3a5547c62c9ba5d51
Alexander Mueller <amueller@doctorcrank.de>
2022-09-19 20:13:26 +0200

    more functions

--- template.sh
+++ template.sh
@@ -1,9 +1,60 @@
 #!/bin/bash
 # Author:       Alexander Mueller
 # Email:        amueller@doctorcrank.de
-# Version:      0.1
-# Date:         2022-03-08
-# Comment:      Please modify this accordingly
-# Description:  This is just a template for scripts
+# Version:      1
+# Date:         2022-07-21
+# Comment:      
+# Description:  This script will generate a new script based on this template
 
-echo "Check out https://github.com/DoctorCr4nk/Skripte!"
+author=$(git config --list | grep "user.name" | cut --delimiter "=" --fields "2")
+email=$(git config --list | grep "user.email" | cut --delimiter "=" --fields "2")
+version="1"
+date=$(date +%Y-%m-%d)
+comment="Wow is this a cool script generator"
+description="Check out my other scripts at https://github.com/DoctorCr4nk"
+path_file_script="${1}"
+arg_quiet=""
+arg_verbose=""
+
+usage ()
+{
+    echo -e "${0} [ARGUMENTS] -f <scriptname>"
+    echo -e ""
+    echo -e ""
+}
+
+if [ -z "${1}" ]
+then
+    echo "Please provide a script name."
+    exit 1
+fi
+
+while getopts "f:h" argument
+do
+    case "${argument}" in
+        f)
+            script_name="${OPTARG}"
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        *)
+        echo "foo"
+        ;;
+    esac
+done
+
+echo "${script_name} will be generated!"
+
+exit 2
+
+echo '#!/bin/bash'                      | tee "${path_file_script}"
+echo '# Author:       '"${author}"      | tee --append "${path_file_script}"
+echo '# Email:        '"${email}"       | tee --append "${path_file_script}"
+echo '# Version:      '"${version}"     | tee --append "${path_file_script}"
+echo '# Date:         '"${date}"        | tee --append "${path_file_script}"
+echo '# Comment:      '"${comment}"     | tee --append "${path_file_script}"
+echo '# Description:  '"${description}" | tee --append "${path_file_script}"
+
+echo "${path_file_script} was created"
