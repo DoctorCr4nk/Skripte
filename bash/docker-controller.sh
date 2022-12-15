c7e038c7082275ec371ecff2bf5a7b84cde126d1
Alexander Mueller <amueller@doctorcrank.de>
2022-12-16 00:50:16 +0100

    'case' instead of 'if' add 'update','restart'

--- bash/docker-controller.sh
+++ bash/docker-controller.sh
@@ -1,29 +1,46 @@
 #!/bin/bash
 ## Author:       Alexander Mueller
 ## Email:        amueller@doctorcrank.de
-## Version:      1.0
+## Version:      1.1
 ## Date:         2022-12-16
 ## Description:  Controll your docker containers
 
 path_docker="/home/amueller/Docker"
-#service_list="changedetection gitea grafana heimdall mailserver qbittorrent redmine nginx-proxy-manager smokeping unifi-controller wikijs"
-service_list="gitea grafana heimdall redmine"
+#container_list="changedetection gitea grafana heimdall mailserver qbittorrent redmine nginx-proxy-manager smokeping unifi-controller wikijs"
+container_list="gitea grafana heimdall redmine"
 
-if [[ ${1} == "up" ]]
-then
-	echo "Start docker container"
-	for service in ${service_list}
-	do
-        	docker-compose --file "${path_docker}/${service}/${service}.yml" up --detach
-	done
-elif [[ ${1} == "down" ]]
-then
-	echo "Stop docker container"
-	for service in ${service_list}
-	do
-        	docker-compose --file "${path_docker}/${service}/${service}.yml" down
-	done
-else
-	echo "Wrong argument, only up or down accepted"
-	exit 1
-fi
+case ${1} in
+	down)
+		echo "Stopping docker containers"
+		for service in ${container_list}
+		do
+				docker-compose --file "${path_docker}/${service}/${service}.yml" down
+		done
+		;;
+	restart)
+		echo "Restarting docker containers"
+		for service in ${container_list}
+		do
+				docker-compose --file "${path_docker}/${service}/${service}.yml" restart
+		done
+		;;
+	up)
+		echo "Starting docker containers"
+		for service in ${container_list}
+		do
+				docker-compose --file "${path_docker}/${service}/${service}.yml" up --detach
+		done
+		;;
+	update)
+		echo "Updating docker containers"
+		for service in ${container_list}
+		do
+				docker-compose --file "${path_docker}/${service}/${service}.yml" down
+				docker-compose --file "${path_docker}/${service}/${service}.yml" pull
+				docker-compose --file "${path_docker}/${service}/${service}.yml" up --detach
+		done
+		;;
+	*)
+		echo "Not a valid argument, please use down, restart, up or update!"
+		exit 1
+esac
