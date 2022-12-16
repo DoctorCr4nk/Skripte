96ae237eb16dd4ed3f494ae3188cb4560ccf0561
Alexander Mueller <amueller@doctorcrank.de>
2022-12-16 01:01:15 +0100

    add --all option

--- bash/docker-controller.sh
+++ bash/docker-controller.sh
@@ -6,8 +6,16 @@
 ## Description:  Controll your docker containers
 
 path_docker="/home/amueller/Docker"
-#container_list="changedetection gitea grafana heimdall mailserver qbittorrent redmine nginx-proxy-manager smokeping unifi-controller wikijs"
-container_list="gitea grafana heimdall redmine"
+container_list_all=$(for dockerfile in $(ls "${path_docker}/"*"/"*".yml"); do echo "${dockerfile}" | cut --delimiter='/' --fields='5'; done)
+container_list_default="changedetection grafana heimdall redmine smokeping"
+
+if [[ "${2}" == '--all' || "${2}" == '-a' ]]
+then
+	container_list="${container_list_all}"
+else
+	container_list="${container_list_default}"
+fi
+echo "Affected containers: ${container_list}"
 
 case ${1} in
 	down)
@@ -15,6 +23,13 @@ case ${1} in
 		for service in ${container_list}
 		do
 				docker-compose --file "${path_docker}/${service}/${service}.yml" down
+		done
+		;;
+	pull)
+		echo "Pulling docker images"
+		for service in ${container_list}
+		do
+				docker-compose --file "${path_docker}/${service}/${service}.yml" pull
 		done
 		;;
 	restart)
@@ -41,6 +56,6 @@ case ${1} in
 		done
 		;;
 	*)
-		echo "Not a valid argument, please use down, restart, up or update!"
+		echo "Not a valid argument, please use down, pull, restart, up or update!"
 		exit 1
 esac
