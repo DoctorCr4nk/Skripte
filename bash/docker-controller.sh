b11021e165dcd1d1c8763a928e9c0e056d319d76
Alexander Mueller <amueller@doctorcrank.de>
2022-12-16 01:09:26 +0100

    variable update, optimize code

--- bash/docker-controller.sh
+++ bash/docker-controller.sh
@@ -5,8 +5,8 @@
 ## Date:         2022-12-16
 ## Description:  Controll your docker containers
 
-path_docker="/home/amueller/Docker"
-container_list_all=$(for dockerfile in $(ls "${path_docker}/"*"/"*".yml"); do echo "${dockerfile}" | cut --delimiter='/' --fields='5'; done)
+path_dir_docker="/home/amueller/Docker"
+container_list_all=$(for path_file_dockerfile in $(ls "${path_dir_docker}/"*"/"*".yml"); do echo "${path_file_dockerfile}" | cut --delimiter='/' --fields='5'; done | xargs)
 container_list_default="changedetection grafana heimdall redmine smokeping"
 
 if [[ "${2}" == '--all' || "${2}" == '-a' ]]
@@ -17,45 +17,30 @@ else
 fi
 echo "Affected containers: ${container_list}"
 
-case ${1} in
-	down)
-		echo "Stopping docker containers"
-		for service in ${container_list}
-		do
-				docker-compose --file "${path_docker}/${service}/${service}.yml" down
-		done
-		;;
-	pull)
-		echo "Pulling docker images"
-		for service in ${container_list}
-		do
-				docker-compose --file "${path_docker}/${service}/${service}.yml" pull
-		done
-		;;
-	restart)
-		echo "Restarting docker containers"
-		for service in ${container_list}
-		do
-				docker-compose --file "${path_docker}/${service}/${service}.yml" restart
-		done
-		;;
-	up)
-		echo "Starting docker containers"
-		for service in ${container_list}
-		do
-				docker-compose --file "${path_docker}/${service}/${service}.yml" up --detach
-		done
-		;;
-	update)
-		echo "Updating docker containers"
-		for service in ${container_list}
-		do
-				docker-compose --file "${path_docker}/${service}/${service}.yml" down
-				docker-compose --file "${path_docker}/${service}/${service}.yml" pull
-				docker-compose --file "${path_docker}/${service}/${service}.yml" up --detach
-		done
-		;;
-	*)
-		echo "Not a valid argument, please use down, pull, restart, up or update!"
-		exit 1
-esac
+for docker_container in ${container_list}
+do
+	path_file_dockerfile="${path_dir_docker}/${docker_container}/${docker_container}.yml"
+	echo "[${docker_container}]"
+	case ${1} in
+		down)
+			docker-compose --file "${path_file_dockerfile}" down
+			;;
+		pull)
+			docker-compose --file "${path_file_dockerfile}" pull
+			;;
+		restart)
+			docker-compose --file "${path_file_dockerfile}" restart
+			;;
+		up)
+			docker-compose --file "${path_file_dockerfile}" up --detach
+			;;
+		update)
+			docker-compose --file "${path_file_dockerfile}" down
+			docker-compose --file "${path_file_dockerfile}" pull
+			docker-compose --file "${path_file_dockerfile}" up --detach
+			;;
+		*)
+			echo "Not a valid argument, please use down, pull, restart, up or update!"
+			exit 1
+	esac
+done
