0ebb06154f73e08733c2026a439a41e7ebebb393
Alexander Mueller <amueller@doctorcrank.de>
2023-03-27 21:36:54 +0200

    small improvements

--- bash/dockerController.sh
+++ bash/dockerController.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 ## Author:       Alexander Mueller
 ## Email:        amueller@doctorcrank.de
-## Version:      1.2
+## Version:      1.3
 ## Date:         2023-01-20
 ## Description:  Controll your docker containers
+##               Will soon be replaced by a python script
 
-path_dir_docker="/home/amueller/Docker"
-container_list_all=$(for path_file_dockerfile in $(ls "${path_dir_docker}/"*"/"*".yml"); do echo "${path_file_dockerfile}" | cut --delimiter='/' --fields='5'; done | xargs)
+path_dir_docker="/Docker/Docker"
+container_list_all=$(for path_file_dockerfile in "${path_dir_docker}/"*"/"*".yml"; do echo "${path_file_dockerfile}" | cut --delimiter='/' --fields='5' | cut --delimiter="." --fields='1'; done | xargs)
 container_list_default=$(cat ${path_dir_docker}/container.list)
 
 if [[ "${2}" == '--all' || "${2}" == '-a' ]]
@@ -15,7 +16,14 @@ then
 else
 	container_list="${container_list_default}"
 fi
-echo "Affected containers: ${container_list}"
+
+if [[ -z "${1}" ]]
+then
+    echo "No arguments provided, please use down, pull, restart, up or update! Aborting!"
+    exit 1
+else
+    echo "Affected containers: ${container_list}"
+fi
 
 for docker_container in ${container_list}
 do
